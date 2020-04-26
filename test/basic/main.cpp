fa9fb45b4ee4d13cc2dc5542ff5e57db2f1f6844
Sean Marks <SeanMarks1123@gmail.com>
2020-04-26 16:21:12 -0400

    Created a custom Array class for numeric types.

--- test/basic/main.cpp
+++ test/basic/main.cpp
@@ -3,11 +3,34 @@
 #include <iostream>
 
 #include <numeric/OpenMP.h>
+#include <numeric/Array.h>
 
 
 int main(int argc, char* argv[])
 {
 	std::cout << "Hello" << "\n";
+
+	const int DIM = 3;
+	numeric::Array<double, DIM> x(5.0);
+
+	numeric::Array<float, DIM> y(2.0);
+	x /= y;
+
+	numeric::Array<double, DIM> y_d = 4.0;
+	x = y;
+
+
+	std::cout << "x=\n";
+	for ( int i=0; i<DIM; ++i ) {
+		std::cout << x[i] << "\n";
+	}
+
+	std::cout << "y=\n";
+	for ( auto it = y.cbegin(); it != y.cend(); ++it ) {
+		std::cout << *it << "\n";
+	}
+
+	std::cout << "y + y_d = " << y + y_d << "\n";
 
 	return 0;
 }
